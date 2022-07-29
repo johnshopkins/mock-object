06ba5e0d982f8545e91f78eec04678d300ea0688
Jen Wachter <jwachter@jhu.edu>
2022-07-29 09:46:20 -0400

    Updated MockObject to allow mocking of interfaces

--- src/MockObject.php
+++ src/MockObject.php
@@ -2,7 +2,9 @@
 
 namespace MockObjectHelper;
 
-class MockObject extends \PHPUnit\Framework\TestCase
+use PHPUnit\Framework\TestCase;
+
+class MockObject extends TestCase
 {
   /**
    * Mock object
@@ -10,15 +12,49 @@ class MockObject extends \PHPUnit\Framework\TestCase
    */
   public $mock;
 
+  protected $mockBuilder;
+
   public function __construct($class, $methods = [], $properties = [])
   {
-    $this->mock = $this->getMockBuilder($class)
-      ->disableOriginalConstructor()
-      ->setMethods(array_keys($methods))
-      ->getMock();
+    $this->mock = !interface_exists($class) ?
+      // class
+      $this->createClassMock($class, $methods) :
+      // interface
+      $this->getMockForAbstractClass($class);
 
     $this->addProperties($properties);
     $this->addMethods($methods);
+  }
+
+  protected function createClassMock($class, $methods)
+  {
+    $this->mockBuilder = $this->getMockBuilder($class);
+    $this->mockBuilder->disableOriginalConstructor();
+
+    // list of methods on class
+    $classMethods = get_class_methods($class);
+    array_shift($classMethods);
+
+    $methodsToAdd = array_keys($methods);
+
+    // list of methods that we're trying to mock that aren't present on the object
+    $add = array_diff($methodsToAdd, $classMethods);
+    if (!empty($add)) {
+      $this->mockBuilder->addMethods($add);
+    }
+
+    // methods we're trying to mock that are already present on the object
+    $only = array_diff($methodsToAdd, $add);
+    if (!empty($only)) {
+      $this->mockBuilder->onlyMethods($only);
+    }
+
+    return $this->mockBuilder->getMock();
+  }
+
+  protected function createInterfaceMock($class)
+  {
+    return $this->getMockForAbstractClass($class);
   }
 
   public function addMethods($methods)
